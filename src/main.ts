07c2f74abbd998d292e72a40f8083cdbb4799dc7
QuanInfinity <kaminari97w@gmail.com>
2025-09-15 11:29:17 +0700

    add localhost for flutter app

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     // credentials: true,
     origin: [
-      'http://localhost:5173',        // React dev server         
+      'http://localhost:51733',        // React dev server         
       'https://smartbox-front-end.vercel.app' // sau này khi deploy frontend
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
